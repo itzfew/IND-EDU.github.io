2668a20eae814c905b9f7ad40e94573a69e65345
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-03-25 11:34:38 +0530

    Update and rename main.js to scripts.js
    Signed-off-by: Itzfew  <113174780+itzfew@users.noreply.github.com>

---
+++ scripts.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const booksCatalogue = document.querySelector('.books-catalogue');
+
+    // Simulated data for demonstration
+    const books = [
+        {
+            title: 'NCERT PUNCH',
+            image: 'img/webshop/NCERT PUNCH Pw.png',
+            description: 'Free Physics Wallah NCERT Punch',
+            downloadLink: 'https://adrinolinks.com/NCERT_PUNCH_2024/'
+        },
+        {
+            title: '36 Years PYQ NEET',
+            image: 'img/webshop/NEET 36 years Pyq.png',
+            description: '36 Years PYQS For Neet.',
+            downloadLink: 'https://adrinolinks.com/33_YEARS_PAPERS_NEET'
+        },
+        // Add more book objects as needed
+    ];
+
+    // Function to create book post HTML
+    function createBookPost(book) {
+        const bookPost = document.createElement('div');
+        bookPost.classList.add('book-post');
+
+        bookPost.innerHTML = `
+            <img src="${book.image}" alt="${book.title}">
+            <p>${book.description}</p>
+            <a href="${book.downloadLink}" class="download-button">Download</a>
+        `;
+
+        booksCatalogue.appendChild(bookPost);
+    }
+
+    // Populate books catalogue
+    books.forEach(book => createBookPost(book));
+});
